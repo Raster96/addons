a68f8e09b17294c2708120db1956389417065bc1
Raster96 <ceinle@o2.pl>
2024-06-02 13:06:53 +0200

    Add files via upload

---
+++ Missing_Party_Members.user.js.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         Missing Party Members
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-29
+// @description  Zaznacza na czerwono graczy w oknie grupy, którzy nie znajdują się na tej samej mapie co my.
+// @author       You
+// @match        https://*.margonem.pl/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
+
+const checkMissingMembers = () => {
+    if (!Engine.party) {
+        return;
+    }
+
+    const currentMapName = Engine.map.d.name;
+    const partyMembers = Object.values(Engine.party.getMembers()).map(member => ({
+        nick: member.nick,
+        div: document.querySelector(`div.party-member.tw-list-item.other-party-id-${member.id}`)
+    }));
+    const hereList = Object.values(Engine.whoIsHere.getList()).map(entry => entry.nick);
+
+    let missingMembers = [];
+
+    partyMembers.forEach(member => {
+        if (member.nick === Engine.hero.d.nick) {
+            return;
+        }
+        if (!hereList.includes(member.nick)) {
+            missingMembers.push(member.nick);
+            if (member.div) {
+                member.div.style.color = 'red';
+            }
+        } else {
+            if (member.div) {
+                member.div.style.color = '';
+            }
+        }
+    });
+
+    if (missingMembers.length > 0) {
+        console.log(`Brakujący gracze na mapie ${currentMapName}: ${missingMembers.join(', ')}`);
+    } else {
+        console.log(`Wszyscy gracze są obecni na ${currentMapName}.`);
+    }
+};
+
+const observePlayerChanges = (selector, callback) => {
+    const observer = new MutationObserver(callback);
+    const targetNode = document.querySelector(selector);
+
+    if (targetNode) {
+        observer.observe(targetNode, { childList: true, subtree: true });
+    } else {
+        console.error(`Nie można znaleźć elementu o selektorze: ${selector}`);
+    }
+};
+
+const startChecking = () => {
+    observePlayerChanges('.who-is-here .players-number', checkMissingMembers);
+    observePlayerChanges('.party .players-number', checkMissingMembers);
+    checkMissingMembers();
+};
+
+const intervalId = setInterval(() => {
+    if (Engine.party) {
+        clearInterval(intervalId);
+        startChecking();
+    }
+}, 1000);
