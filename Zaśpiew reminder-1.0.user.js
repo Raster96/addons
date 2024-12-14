9af07ad3835510ee24871c9b34221a1e3e69b153
Raster96 <rast96r@gmail.com>
2024-12-14 17:19:51 +0100

    Add files via upload

---
+++ Zaśpiew reminder-1.0.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Zaśpiew reminder
+// @version      1.0
+// @author       You
+// @description  Po rozpoczęciu walki z tytanem wyświetla żółty komunikat przypominający o użyciu zaśpiewów (Mroczny Wrzask i Krzyk z Otchłani)
+// @match        *.margonem.pl/
+// @exclude      https://www.margonem.pl/
+// @exclude      https://www.margonem.pl//*.
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const intercept = (obj, key, cb, _ = obj[key]) => obj[key] = (...args) => {
+        const result = _.apply(obj, args);
+        return cb(...args) ?? result;
+    };
+
+    intercept(Engine.communication, 'parseJSON', (data) => {
+        if (data.f && data.npcs && data.npcs[0] && data.npcs[0].wt > 99) {
+            const items = Engine.items.fetchLocationItems("g");
+
+            if (items && Array.isArray(items)) {
+                items.forEach(item => {
+                    if (item.name === "Mroczny Wrzask" || item.name === "Krzyk z Otchłani") {
+                        message(`Przypomnienie o użyciu przedmiotu ${item.name}.`);
+                    }
+                });
+            }
+        }
+    });
+})();
