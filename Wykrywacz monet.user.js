1046c8d3244bcfc5f434e147e518cdcc03146bc8
Raster96 <rast96r@gmail.com>
2024-12-18 13:42:32 +0100

    Add files via upload

---
+++ Wykrywacz monet.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Wykrywacz monet
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-18
+// @description  Wyświetla żółty komunikat i wyszukuje "Moneta" na minimapach.
+// @author       You
+// @match        http*://*.margonem.pl/
+// @exclude      http*://www.margonem.pl/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function checkNpcs(npc) {
+        if (npc.d.nick === 'Moneta') {
+            message('Moneta!');
+
+            const searchInput = document.querySelector('.search-wrapper.search-item-wrapper .search');
+            if (searchInput) {
+                searchInput.value = 'Moneta';
+                searchInput.dispatchEvent(new Event('keyup', { bubbles: true }));
+            }
+
+            const bottomBarInput = document.querySelector('.mmpWrapper .mmpBottombar input');
+            if (bottomBarInput) {
+                bottomBarInput.value = 'Moneta';
+                bottomBarInput.dispatchEvent(new Event('keyup'));
+            }
+        }
+    }
+
+    window.API.addCallbackToEvent('newNpc', function(npc) {
+        checkNpcs(npc);
+    });
+})();
