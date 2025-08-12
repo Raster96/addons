64f50dc1023dba8ab57c00bfd31edfe4dc1fa0eb
Raster96 <rast96r@gmail.com>
2025-08-12 14:38:22 +0200

    Update Zapraszanie_pod_V_na_NI.user.js

--- Zapraszanie_pod_V_na_NI.user.js
+++ Zapraszanie_pod_V_na_NI.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Zapraszanie pod "V" na NI (losowanie)
 // @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  Edycja dodatku autorstwa Arhq, który zapraszał wszystkich graczy z mapy. Dodatek zaprasza jedną (losową) osobę z mapy po kliknięciu "V" i zmienia kolor nicku zapraszanego gracza na "Gracze na mapie" dopóki nie zaakceptuje zaproszenia. W przypadku gdy zapraszany gracz znajduje się w innej grupie lub gdy ma inne niezaakceptowane zaproszenie, zaproszenie zostanie wysłane do kolejnego gracza.
-// @author       You (edycja oryginalnego kodu autorstwa Arhq)
+// @version      1.5
+// @description  Edycja dodatku autorstwa Arhq — zaprasza jedną losową osobę po wciśnięciu "V" i zmienia kolor nicku do momentu zaakceptowania zaproszenia.
+// @author       You
 // @match        http*://*.margonem.pl/
 // @exclude      http*://www.margonem.pl/
 // @icon         https://www.google.com/s2/favicons?domain=margonem.pl
@@ -19,27 +19,18 @@
                 ["cl", "fr", "cl-fr"].includes(relation)
             );
 
-            const tryInvite = (remainingList) => {
-                if (remainingList.length === 0) return;
+            if (list.length === 0) return;
 
-                const [id] = remainingList[Math.floor(Math.random() * remainingList.length)];
+            const [id] = list[Math.floor(Math.random() * list.length)];
 
-                _g(`party&a=inv&id=${id}`, (data) => {
-                    if (data.message) {
-                        const element = document.querySelector(`div[data-id="${id}"] .center`);
-                        if (element) {
-                            element.style.color = "rgb(255, 0, 0)";
-                        }
-                    } else {
-                        const updatedList = remainingList.filter(([otherId]) => otherId !== id);
-                        tryInvite(updatedList);
+            _g(`party&a=inv&id=${id}`, (data) => {
+                if (data.message) {
+                    const element = document.querySelector(`div[data-id="${id}"] .center`);
+                    if (element) {
+                        element.style.color = "rgb(255, 0, 0)";
                     }
-                });
-            };
-
-            if (list.length > 0) {
-                tryInvite(list);
-            }
+                }
+            });
         }
     });
 })();
