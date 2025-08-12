41a867f277cbbeddd9ad6cac9ced766d89e83359
Raster96 <rast96r@gmail.com>
2025-08-12 14:18:03 +0200

    Update Loot w jednej linii-1.0.user.js

--- Loot w jednej linii-1.0.user.js
+++ Loot w jednej linii-1.0.user.js
@@ -3,12 +3,12 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Loot w jednej linii
-// @match        *://*.margonem.pl/*
-// @exclude      https://www.margonem.pl/
-// @exclude      https://www.margonem.pl//*.
+// @match        http*://*.margonem.pl/
+// @exclude      http*://www.margonem.pl/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
 // @grant        GM_addStyle
 // ==/UserScript==
+
 
 (function() {
     'use strict';
