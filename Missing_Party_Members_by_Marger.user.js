188968d8efe7577593d11dbea025702a77c5b6ad
Raster96 <rast96r@gmail.com>
2025-12-08 11:35:07 +0100

    Refactor Missing Party Members script for updates
    Updated the script to exclude specific URLs and improved party member checks.

--- Missing_Party_Members_by_Marger.user.js
+++ Missing_Party_Members_by_Marger.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Missing Party Members
 // @namespace    http://tampermonkey.net/
-// @version      2024-08-25
+// @version      2025-12-08
 // @description  Zaznacza na czerwono w oknie grupy graczy, którzy nie znajdują się w zasięgu 20 kratek od nas.
 // @author       Marger
 // @match        http*://*.margonem.pl/
+// @exclude      http*://www.margonem.pl/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
 // @grant        none
 // ==/UserScript==
@@ -16,16 +17,18 @@ const isOtherInBattleRange = (other) => {
     return Math.max(Math.abs(x - hx), Math.abs(y - hy)) <= 20;
 };
 const updatePartyMembers = () => {
-    if (!Engine.party) return;
+    if (!Engine.party || !Engine.party.isPartyPlayers()) return;
     const others = Engine.others.check();
     const members = Engine.party.getMembers();
     const { id: hid } = Engine.hero.d;
-    for (const id of Object.keys(members)) {
-        if (id == hid) continue;
-        const $nickname = members[id].$.find('.nickname');
+
+    members.forEach((member, id) => {
+        if (id == hid) return;
+        const $el = $(member.el);
+        const $nickname = $el.find('.nickname .nickname-text');
         const inRange = others[id] && isOtherInBattleRange(others[id]);
         $nickname.css('color', inRange ? '' : 'red');
-    }
+    });
 };
 const intercept = (obj, key, cb, _ = obj[key]) => obj[key] = (...args) => {
     const result = _.apply(obj, args);
